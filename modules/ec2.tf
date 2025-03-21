eece7384317571c7d4d85ce7b58ebbe55eb6af89
n-morishita <n.morishita.gxp@gmail.com>
2025-03-21 09:53:51 +0900

    軽微な修正

--- modules/ec2.tf
+++ modules/ec2.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "web" {
   instance_type          = "t2.small"
   subnet_id              = aws_subnet.private["az_a"].id
   vpc_security_group_ids = [aws_security_group.web.id]
-  user_data              = file("${path.module}/user_data/app-setup.sh")
+  user_data              = file("${path.module}/modules/user_data/app-setup.sh")
   iam_instance_profile   = aws_iam_instance_profile.ssm_instance_profile.name
 
   tags = {
