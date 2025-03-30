5cb326121a1213adfbe2968fd8baf77f8afc9159
n.morishita <n.morishita.gxp@gmail.com>
2025-03-30 17:00:53 +0900

    ssm用のvpcエンドポイントを追加

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "study-s3-tfstate"  # Terraformの状態ファイル（tfstate）を保存するS3バケット名(事前に作成)
+    bucket = "morishita-study-s3-tfstate"  # Terraformの状態ファイル（tfstate）を保存するS3バケット名(事前に作成)
     region = "ap-northeast-1"    # S3バケットのリージョン（東京リージョン）
     key    = "study/terraform.tfstate" # S3バケット内でのtfstateファイルの保存パス
   }
