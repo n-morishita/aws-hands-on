5ad7d0d67baf29344653c4805db8360b1e3d871e
n.morishita <n.morishita.gxp@gmail.com>
2025-10-11 22:46:56 +0900

    20251011 terraformブロックを移動

--- ecs/fargate/provider.tf
+++ ecs/fargate/provider.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 1.13.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-
 provider "aws" {
   region = "ap-northeast-1"
   
